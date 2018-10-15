682923b4ca8ba33ae94a2ef742797fc5f09fece3
k-bentsen <kbentsen@gmail.com>
2018-10-14 18:53:15 -0700

    start lightning cmp

---
+++ force-app/main/default/aura/GrantPermissionSet/GrantPermissionSetController.js
@@ -0,0 +1,6 @@
+({
+    userUpdate : function(component, event, helper) {
+        console.log(component.find("userId").get("v.value"));
+        //console.log(component.get("v.permSetGrant"));
+    }
+})
