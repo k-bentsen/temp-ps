03c4e7629f3edb1478efeed37a59c6ba5a5d253e
kyle bentsen <kbentsen@gmail.com>
2018-10-16 00:30:23 -0700

    populate perm set list

--- force-app/main/default/aura/GrantPermissionSet/GrantPermissionSetController.js
+++ force-app/main/default/aura/GrantPermissionSet/GrantPermissionSetController.js
@@ -1,6 +1,21 @@
 ({
-    userUpdate : function(component, event, helper) {
-        console.log(component.find("userId").get("v.value"));
-        //console.log(component.get("v.permSetGrant"));
+    onInit : function(cmp, event, helper){
+        var queryPS = cmp.get("c.getPermissionSets");
+        queryPS.setCallback(this, function(resp){
+            if(resp.getState() === "SUCCESS"){
+                cmp.set("v.permSets", resp.getReturnValue());
+            }
+        });
+        $A.enqueueAction(queryPS);
+    },
+
+    userUpdate : function(cmp, event, helper){
+        var userVal = cmp.find("userId").get("v.value");
+        console.log(userVal);
+        console.log(userVal != "");
+    },
+
+    permSetListChange : function(cmp, event, helper){
+        console.log(cmp.find("permSetId").get("v.value"));
     }
 })
