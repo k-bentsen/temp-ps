aa3fc07902e85f2fe4d7321657c4ba7f618d95cd
kyle bentsen <kbentsen@gmail.com>
2018-10-17 22:29:22 -0700

    create grant form w/ controllers & validation

--- force-app/main/default/aura/GrantPermissionSet/GrantPermissionSetController.js
+++ force-app/main/default/aura/GrantPermissionSet/GrantPermissionSetController.js
@@ -2,6 +2,8 @@
     onInit : function(cmp, event, helper){
         var queryPS = cmp.get("c.getPermissionSets");
         queryPS.setCallback(this, function(resp){
+            helper.clearFields(cmp);
+            helper.buttonToggle(cmp);
             if(resp.getState() === "SUCCESS"){
                 cmp.set("v.permSets", resp.getReturnValue());
             }
@@ -11,11 +13,71 @@
 
     userUpdate : function(cmp, event, helper){
         var userVal = cmp.find("userId").get("v.value");
-        console.log(userVal);
-        console.log(userVal != "");
+        var queryPS = cmp.get("c.getPermissionSets");
+        if(userVal != ""){
+            queryPS.setParams({userId : userVal});
+        } else{
+            cmp.find("permSetId").set("v.value", "");
+        }
+        helper.buttonToggle(cmp);
+        queryPS.setCallback(this, function(resp){
+            if(resp.getState() === "SUCCESS"){
+                cmp.set("v.permSets", resp.getReturnValue());
+            }
+        });
+        $A.enqueueAction(queryPS);
     },
 
-    permSetListChange : function(cmp, event, helper){
-        console.log(cmp.find("permSetId").get("v.value"));
+    insertGrant : function(cmp, event, helper){
+        var spinner = cmp.find("grantSpinner");
+        $A.util.toggleClass(spinner, "slds-hide");
+
+        var createGrant = cmp.get("c.insertPermSetGrant");
+        createGrant.setParams({
+            userId : cmp.find("userId").get("v.value"),
+            permSetId : cmp.find("permSetId").get("v.value"),
+            dur : cmp.find("dur").get("v.value"),
+            durUnit : cmp.find("durUnit").get("v.value")
+        });
+        createGrant.setCallback(this, function(resp){
+            $A.util.toggleClass(spinner, "slds-hide");
+            var toastEvent = $A.get("e.force:showToast");
+            if(resp.getState() === "SUCCESS"){
+                toastEvent.setParams({
+                    "type" : "success",
+                    "title": "Success!",
+                    "message": "Grant successful"
+                });
+                
+                helper.clearFields(cmp);
+                helper.buttonToggle(cmp);
+            } else{
+                toastEvent.setParams({
+                    "type" : "error",
+                    "title": "Error",
+                    "message": resp.getError()
+                });
+            }
+            toastEvent.fire();
+        });
+        $A.enqueueAction(createGrant);
+    },
+
+    inputChange : function(cmp, event, helper){
+        helper.buttonToggle(cmp);
     }
+    
+    /* difficulty displaying pop-overs; nice-to-have, save for later
+
+    toggleBtnPopover : function(cmp, event, helper){
+        console.log("in the toggle method");
+        cmp.find('btnPopover').showCustomPopover({
+            body: "Popovers are positioned relative to a reference element",
+            referenceSelector: ".btnElem",
+            cssClass: "popoverclass,no-pointer"
+        }).then(function(overLay){
+            console.log(overLay);
+        });
+    },
+    */   
 })
--- force-app/main/default/aura/GrantPermissionSet/GrantPermissionSetHelper.js
+++ force-app/main/default/aura/GrantPermissionSet/GrantPermissionSetHelper.js
@@ -1,5 +1,22 @@
 ({
-    helperMethod : function() {
+    buttonToggle : function(cmp) {
+        var user = cmp.find("userId").get("v.value");
+        var permSetId = cmp.find("permSetId").get("v.value");
+        var dur = cmp.find("dur").get("v.value");
+        var durUnit = cmp.find("durUnit").get("v.value");
+        console.log("user id: " + user);
+        console.log("SP id: " + permSetId);
+        console.log("duration: " + dur);
+        console.log("duration unit: " + durUnit);
+        if(user == "" || permSetId == "" || dur == "" || durUnit == ""){
+            cmp.set("v.btnDisabled", "true");
+        } else{
+            cmp.set("v.btnDisabled", "false");
+        }
+    },
 
+    clearFields : function(cmp){
+        cmp.find("userId").set("v.value", null);
+        cmp.find("permSetId").set("v.value", "");
     }
 })
